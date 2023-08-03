9aed00abd8657c25f29dc448f06bade39f90032c
uijin <abrahamkim98@gmail.com>
2023-08-03 10:50:54 +0900

    clean internimage

--- mmseg_1.x.x/work_dirs/swin/swin90.19_second_Train_config.py
+++ mmseg_1.x.x/work_dirs/swin/swin90.19_second_Train_config.py
@@ -259,7 +259,7 @@ visualizer = dict(
     name='visualizer')
 log_processor = dict(by_epoch=False)
 log_level = 'INFO'
-load_from = 'work_dirs/swin-large-patch4-window12-in22k-384x384-pre_upernet_8xb2-160k_ade20k-512x512/iter_160000.pth'
+load_from = 'swin90.19_first_Train_iter_158000.pth' #google drive link에서 다운후 가능합니다.
 resume = False
 tta_model = dict(type='SegTTAModel')
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
